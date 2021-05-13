d75063c0c4b4022655854b2b21b9d6dff8d42fb2
Bpham1 <brandon.pham@utexas.edu>
2021-05-12 22:23:15 -0500

    Fixed Top Down Sort Order and Added Player

---
+++ BasicTopDownMechanics/Assets/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    public float moveSpeed = 5f;
+
+    Vector2 movement = new Vector2(0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement.x < 0 && facingRight)
+        {
+            facingRight = false;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (movement.x > 0 && !facingRight)
+        {
+            facingRight = true;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Move player
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
