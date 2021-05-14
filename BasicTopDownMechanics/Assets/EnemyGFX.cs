060842fc29dbedd6a2bead4356a6fe479a3d8d8e
Bpham1 <brandon.pham@utexas.edu>
2021-05-13 22:00:01 -0500

    Adjusted Pathfinding and added rotation script to Enemy

---
+++ BasicTopDownMechanics/Assets/EnemyGFX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyGFX : MonoBehaviour
+{
+    public AIPath aiPath;
+    private bool facingRight = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aiPath.desiredVelocity.x < 0 && facingRight) {
+            facingRight = false;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (aiPath.desiredVelocity.x > 0 && !facingRight) {
+            facingRight = true;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
